1447ae306a13a92c2b0d6b33652d8902e66c365b
thong2085 <129254706+thong2085@users.noreply.github.com>
2023-09-29 22:30:36 +0700

    create header and css

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,13 +1,30 @@
 import classNames from "classnames/bind";
 import styles from "./Header.module.scss";
+import images from "../../../../assets/images";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass, faSpinner, faXmarkCircle } from "@fortawesome/free-solid-svg-icons";
 
 const cx = classNames.bind(styles);
-
 function Header() {
   return (
     <header className={cx("wrapper")}>
       <div className={cx("inner")}>
-        <div className={cx("logo")}></div>
+        <div className={cx("logo")}>
+          <img src={images.logo} alt="logo" />
+        </div>
+        <div className={cx("search")}>
+          <form className={cx("search-form")} method="GET">
+            <input type="text" placeholder="Search" />
+            <button className={cx("clear-btn")}>
+              <FontAwesomeIcon icon={faXmarkCircle} />
+            </button>
+            <FontAwesomeIcon className={cx("loading")} icon={faSpinner} />
+            <button className={cx("search-btn")}>
+              <FontAwesomeIcon icon={faMagnifyingGlass} />
+            </button>
+          </form>
+        </div>
+        <div className={cx("actions")}></div>
       </div>
     </header>
   );
